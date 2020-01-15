5695027324ee91ecb6fae6c9bc24b70332da9ab6
Brewer <ahcbrewer@users.noreply.github.com>
2020-01-15 01:18:22 -0500

    prototype

---
+++ New Unity Project/Assets/playermovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) 
+	{
+		Vector3 newRotation = transform.rotation.eulerAngles;
+		newRotation.y += 10;
+		transform.rotation = Quaternion.Euler (newRotation);
+	}    
+	else if (Input.GetKey(KeyCode.LeftArrow)) 
+	{
+		Vector3 newRotation = transform.rotation.eulerAngles;
+		newRotation.y -= 10;
+		transform.rotation = Quaternion.Euler (newRotation);
+	}
+	if (Input.GetKey(KeyCode.UpArrow)) 
+	{
+		GetComponent<Rigidbody>().AddForce(transform.forward * 1000 * Time.deltaTime);
+	}
+    }
+}
